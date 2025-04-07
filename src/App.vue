48192524d0356a71473e67f07c6d385d9db2f4ca
程程 <huayuprophet@gmail.com>
2025-04-07 13:59:37 +0800

    倒计时

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         </div>
         <div>
             <span>倒计时：</span>
-            <span></span>
+            <span>{{ }}</span>
 
         </div>
         <div>
@@ -38,9 +38,12 @@
 <script setup>
 import { ElConfigProvider } from 'element-plus';
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, provide, computed } from 'vue';
 // 当前时间
 const now = ref(0)
+//提供给组件
+provide('now', now)
+//实时更新时间
 now.value = Date.now()
 function now_timeout() {
     setTimeout(() => {
@@ -53,28 +56,41 @@ onMounted(() => {
 
 })
 
+//倒计时
+const timer = ref({
+    time_0: 1,
+    time: 6,
+})
+const time_1 = computed(() => {
+    return timer.value.time_0 + timer.value.time
+})
+const tim
+// timer.value.time_0 + timer.value.time
+
+
 // 快捷设置倒计时
-const timing = ref('')
+const count_setting = ref('')
 function timing_submit() {
-    let str = timing.value
+    let str = count_setting.value
     // 使用正则表达式 /\s+/ 来匹配一个或多个空格
     let strArray = str.split(/\s+/);
     let numArray = strArray.map(Number);
     let len = numArray.length;
     let after
     if (len === 1) {
-        let m = timing.value[0] * 60
+        let m = numArray.value[0] * 60
         after = m
     } else if (len === 2) {
-        let m = timing.value[0] * 60
-        let s = timing.value[1]
+        let m = numArray.value[0] * 60
+        let s = numArray.value[1]
         after = m + s
     } else if (len === 3) {
-        let h = timing.value[0] * 360
-        let m = timing.value[1] * 60
-        let s = timing.value[2]
+        let h = numArray.value[0] * 360
+        let m = numArray.value[1] * 60
+        let s = numArray.value[2]
         after = h + m + s
-    }
-
+    } else { }
+    timer.value.time_0 = now.value
+    timer.value.time = after
 }
 </script>
